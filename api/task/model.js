d13e5579c5d0fa32e2f092cdc53719af793ddfdd
rkshockey <rkshockey@gmail.com>
2021-09-10 11:30:29 -0400

    Completed tasks path

--- api/task/model.js
+++ api/task/model.js
@@ -1,11 +1,21 @@
-
+const db = require('../../data/dbConfig')
 
 async function findAll () {
-    return 'task findAll wired'
+    const rows = await db('tasks');
+    return rows.map(item => {return {
+        ...item,
+        task_completed: item.task_completed ? true : false
+    }});
+}
+
+async function findById (id) {
+    const row = await db('tasks').where('task_id', id).first()
+    return {...row, task_completed: row.task_completed ? true : false}
 }
 
 async function insert (task) {
-    return 'task insert wired'
+    const [id] = await db('tasks').insert(task)
+    return findById(id)
 }
 
 module.exports = { findAll, insert }
