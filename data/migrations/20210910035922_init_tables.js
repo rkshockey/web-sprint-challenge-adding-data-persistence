b5a4d4eee378a78d9e8538d5ec7489e92c3f6a87
rkshockey <rkshockey@gmail.com>
2021-09-10 07:31:45 -0400

    Added columns to projects table

--- data/migrations/20210910035922_init_tables.js
+++ data/migrations/20210910035922_init_tables.js
@@ -3,6 +3,9 @@ exports.up = async function(knex) {
   return await knex.schema
     .createTable('projects', table => {
         table.increments('project_id');
+        table.string('project_name').notNullable();
+        table.string('project_description');
+        table.boolean('project_completed').defaultTo(false);
     })
     .createTable('resources', table => {
         table.increments('resource_id');
